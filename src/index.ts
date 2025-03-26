7c99005db8c75883f967de44152d4e3a5bf93452
inyourtime <inyourtimeguy@gmail.com>
2025-03-26 09:40:18 +0700

    chore: add host on start server

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ app.get("/", async () => {
   return { hello: "world" };
 });
 
-app.listen({ port: 3000 }, (err, address) => {
+app.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
